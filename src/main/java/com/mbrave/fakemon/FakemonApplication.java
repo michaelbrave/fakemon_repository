01653176af605fd0562e4f008d52826d4d3e33e7
Michael Brave <michaelbrave@gmail.com>
2021-03-15 14:01:52 -0500

    boilerplate

---
+++ src/main/java/com/mbrave/fakemon/FakemonApplication.java
@@ -0,0 +1,13 @@
+package com.mbrave.fakemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbrave/fakemon/FakemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbrave.fakemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
